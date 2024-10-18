bcfc8af054d8b5aa5c0c5c1d8ae423db4fc0d43b
zolinthecow <colinzhao777@gmail.com>
2024-10-17 17:45:43 -0700

    tailwind

--- studio/src/routes/index.tsx
+++ studio/src/routes/index.tsx
@@ -1,25 +1,29 @@
-import { Title } from "@solidjs/meta";
-import { createAsync } from "@solidjs/router";
-import Counter from "~/components/Counter";
-import { api } from "~/lib/api";
+import { Title } from '@solidjs/meta';
+import { createAsync } from '@solidjs/router';
+import Counter from '~/components/Counter';
+import { api } from '~/lib/api';
 
 export default function Home() {
-  const hello = createAsync(() => api.example.hello.query("world"));
-  return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-      <pre>
-        <code>{JSON.stringify(hello(), null, 2)}</code>
-      </pre>
-    </main>
-  );
+    const hello = createAsync(() => api.example.hello.query('world'));
+    return (
+        <main>
+            <Title>Hello World</Title>
+            <h1 class="text-purple-300 text-2xl">Hello world!</h1>
+            <Counter />
+            <p>
+                Visit{' '}
+                <a
+                    href="https://start.solidjs.com"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    start.solidjs.com
+                </a>{' '}
+                to learn how to build SolidStart apps.
+            </p>
+            <pre>
+                <code>{JSON.stringify(hello(), null, 2)}</code>
+            </pre>
+        </main>
+    );
 }
