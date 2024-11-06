8d4f778a8a8dd0a9209f2775a6a868666f68681a
zolinthecow <colinzhao777@gmail.com>
2024-11-06 02:35:47 +0000

    dbos example should work?

--- js/examples/dbos.ts
+++ js/examples/dbos.ts
@@ -1,5 +1,12 @@
 // DBOS Durable Agent
-import { Step, type StepContext } from '@dbos-inc/dbos-sdk';
+import {
+    type HandlerContext,
+    PostApi,
+    Step,
+    type StepContext,
+    Workflow,
+    type WorkflowContext,
+} from '@dbos-inc/dbos-sdk';
 import { z } from 'zod';
 import ProgramState, { OpenAIBackend, createTools } from '../src/index.js';
 
@@ -9,14 +16,22 @@ const ProcessRefundArgsSchema = z.object({
     _reason: z.string().optional(),
 });
 
+// biome-ignore lint/complexity/noStaticOnlyClass: DBOS requires this
 class RefundAgent {
-    static processRefund(args: z.infer<typeof ProcessRefundArgsSchema>) {
+    @Step()
+    static async processRefund(
+        ctx: StepContext,
+        args: z.infer<typeof ProcessRefundArgsSchema>,
+    ) {
         const reason = args._reason ?? 'NOT SPECIFIED';
         console.log(
             `[mock] Refunding ${args.userName} for item ${args.itemID}, because ${reason}...`,
         );
         for (let i = 1; i <= 6; i++) {
             RefundAgent.refundStep(i);
+            await new Promise((resolve) =>
+                setTimeout(() => resolve('done'), 1000),
+            );
         }
         return 'Success!';
     }
@@ -28,74 +43,85 @@ class RefundAgent {
         return 'Processed';
     }
 
-    // @Step()
-    static applyDiscount() {
+    @Step()
+    static async applyDiscount(ctx: StepContext) {
         console.log('[mock] Applying discount...');
         return 'Applied discount of 11%';
     }
 
-    foo() {
-        return 'bar';
+    @Workflow()
+    static async RefundWorkflow(
+        ctx: WorkflowContext,
+        userName: string,
+        userRequest: string,
+    ) {
+        const s = new ProgramState(
+            new OpenAIBackend({ apiKey: process.env.OPENAI_API_KEY }),
+        );
+        s.setModel('gpt-4o-mini');
+
+        const tools = createTools([
+            {
+                function: (args: z.infer<typeof ProcessRefundArgsSchema>) =>
+                    ctx.invoke(RefundAgent).processRefund(args),
+                name: 'processRefund',
+                description: 'Processes a refund request',
+                params: ProcessRefundArgsSchema,
+            },
+            {
+                function: () => ctx.invoke(RefundAgent).applyDiscount(),
+                name: 'applyDiscount',
+                description:
+                    'Provides a coupon for an 11% discount on their next order',
+            },
+        ]);
+
+        s.add(
+            s.system`${
+                'You are a helpful agent currently helping a customer process a refund. Additionally, If the reason is that it was too expensive,' +
+                'also offer the user a discount code along with the refund. Always process the refund.'
+            }`,
+        ).add(s.user`From ${userName}: ${userRequest}`);
+
+        const iterations = 0;
+        while (iterations < 10) {
+            await s.add(
+                s.assistant`${s.gen('action', {
+                    tools,
+                })}`,
+            );
+            const action = s.get('action', { from: 'tools', tools });
+            if (!action) {
+                console.error(`No tool was used: ${action}`);
+                return;
+            }
+            const responseToUser = action.find(
+                (a) => a.toolUsed === 'respondToUser',
+            );
+            if (responseToUser) {
+                const resp = responseToUser.response ?? '';
+                console.log(resp);
+                return resp;
+            }
+            for (const toolUsed of action) {
+                s.add(
+                    s.user`${toolUsed.toolUsed} Result: ${JSON.stringify(toolUsed.response)}`,
+                );
+            }
+        }
+        return 'Failed to apply discount';
+    }
+
+    @PostApi('/refund')
+    static async startRefund(ctx: HandlerContext) {
+        const userName = 'Max';
+        const userRequest =
+            "I want to refund item 99 because it's too expensive and I don't " +
+            'like its color! I want to proceed with the refund and also get a discount ' +
+            'for my next purchase!';
+        const resp = await ctx
+            .startWorkflow(RefundAgent)
+            .RefundWorkflow(userName, userRequest);
+        return resp;
     }
 }
-
-(async () => {
-    // const s = new ProgramState();
-    // await s.setModel('http://localhost:30000');
-    const s = new ProgramState(
-        new OpenAIBackend({ apiKey: process.env.OPENAI_API_KEY }),
-    );
-    s.setModel('gpt-4o-mini');
-    // s.beginDebugRegion({ debugName: "Tool calling" });
-
-    const tools = createTools([
-        {
-            function: RefundAgent.applyDiscount,
-            name: 'applyDiscount',
-            description: 'Provide a discount of 11% on their next order',
-        },
-        {
-            function: RefundAgent.processRefund,
-            name: 'processRefund',
-            params: ProcessRefundArgsSchema,
-            description: 'Process a refund request',
-        },
-    ]);
-
-    s.add(
-        s.system`${
-            'You are a helpful agent currently helping a customer process a refund. Additionally, If the reason is that it was too expensive,' +
-            'also offer the user a discount code along with the refund. Always process the refund.'
-        }`,
-    ).add(
-        s.user`${
-            "From Max: I want to refund item 99 because it's too expensive and I don't " +
-            'like its color! I want to proceed with the refund and also get a discount ' +
-            'for my next purchase!'
-        }`,
-    );
-
-    while (true) {
-        await s.add(
-            s.assistant`${s.gen('action', {
-                tools,
-            })}`,
-        );
-        const action = s.get('action', { from: 'tools', tools });
-        if (!action) {
-            console.error(`No tool was used: ${action}`);
-            return;
-        }
-        if (action.some((a) => a.toolUsed === 'respondToUser')) {
-            console.log(
-                action.find((a) => a.toolUsed === 'respondToUser')?.response,
-            );
-            break;
-        }
-        for (const toolUsed of action) {
-            s.add(
-                s.user`${toolUsed.toolUsed} Result: ${JSON.stringify(toolUsed.response)}`,
-            );
-        }
-    }
-})();
